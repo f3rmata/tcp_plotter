e6c58b82ee086689ca0dc48408cf40a5c228f038
fermata <fermata@h2o.moe>
2025-03-21 19:54:07 +0800

    TODO

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,30 @@
 pub mod tcp_receiver {
-    use std::{error::Error, net::SocketAddr};
     use serde::{Deserialize, Serialize};
+    use std::{
+        error::Error,
+        net::{Ipv4Addr, SocketAddr},
+    };
     use tokio::{
         io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
         net::{TcpListener, TcpStream},
     };
 
     #[derive(Debug, Serialize, Deserialize)]
-    struct Cordinate {
+    pub struct Cordinate {
         x: f64,
         y: f64,
     }
-    
+
     impl Cordinate {
-        fn new(x: f64, y: f64) -> Cordinate {
+        pub fn new(x: f64, y: f64) -> Cordinate {
             Cordinate { x, y }
         }
     }
-    
-    async fn open_socket(server_ip: String, server_port: u16)
-                   -> Result<(TcpStream, SocketAddr), Box<dyn Error>> {
 
+    async fn open_socket(
+        server_ip: Ipv4Addr,
+        server_port: u16,
+    ) -> Result<(TcpStream, SocketAddr), Box<dyn Error>> {
         // TODO: added ipv6 support
         let listener = TcpListener::bind((server_ip, server_port)).await?;
         println!("Server listening on port {server_port}");
@@ -33,8 +37,6 @@ pub mod tcp_receiver {
         mut socket: TcpStream,
         addr: SocketAddr,
     ) -> Result<(), Box<dyn Error>> {
-        // 创建带缓冲的读取器
-
         let mut reader = BufReader::new(&mut socket);
         let mut json_data = String::new();
 
@@ -42,12 +44,8 @@ pub mod tcp_receiver {
         reader.read_line(&mut json_data).await?;
 
         // 解析JSON数据
-        let sensor_data: Cordinate = serde_json::from_str(&json_data)?;
-
-        println!(
-            "Received from {}: {:?}",
-            addr, sensor_data
-        );
+        let cordinate_data: Cordinate = serde_json::from_str(&json_data)?;
+        println!("Received from {}: {:?}", addr, cordinate_data);
 
         // 发送响应
         socket
@@ -56,9 +54,8 @@ pub mod tcp_receiver {
 
         Ok(())
     }
-    
-    #[tokio::main]
-    async fn tcp_server(server_ip: String, server_port: u16) -> Result<(), Box<dyn Error>> {
+
+    pub async fn tcp_server(server_ip: Ipv4Addr, server_port: u16) -> Result<(), Box<dyn Error>> {
         let (socket, addr) = open_socket(server_ip, server_port).await?;
         tokio::spawn(async move {
             if let Err(e) = handle_connection(socket, addr).await {
@@ -66,6 +63,62 @@ pub mod tcp_receiver {
             }
         });
 
+        Ok(())
+    }
+}
+
+pub mod plot {
+    use std::error::Error;
+    use slint::{Rgb8Pixel, SharedPixelBuffer};
+    use plotters::{prelude::*, style::full_palette::BLUEGREY};
+
+    pub fn call_plotter(pixel_buffer: &mut SharedPixelBuffer<Rgb8Pixel>) -> Result<(), Box<dyn Error>>{
+        // TODO: add error handling rather than panic.
+        let foreground = RGBAColor(255, 255, 255, 0.8);
+        let background = RGBAColor(40, 40, 40, 1.0);
+    
+        let size = (pixel_buffer.width(), pixel_buffer.height());
+        let backend = BitMapBackend::with_buffer(pixel_buffer.make_mut_bytes(), size);
+    
+        let root = backend.into_drawing_area();
+        root.fill(&background).expect("error filling drawing area");
+        let mut chart = ChartBuilder::on(&root)
+            .margin(20)
+            .x_label_area_size(80)
+            .y_label_area_size(80)
+            .build_cartesian_2d(-1f32..1f32, -0.1f32..1f32)
+            .expect("error building chart...");
+    
+        chart
+            .configure_mesh()
+            .axis_style(&foreground)
+            .label_style(("sans-serif", 20).into_font().color(&foreground))
+            .bold_line_style(&foreground.mix(0.15))
+            .light_line_style(&foreground.mix(0.05))
+            .draw()
+            .expect("error drawing...");
+    
+        chart
+            .draw_series(LineSeries::new(
+                (-50..=50).map(|x| x as f32 / 50.0).map(|x| (x, x * x)),
+                ShapeStyle::from(&BLUEGREY).stroke_width(3),
+            ))
+            .expect("error drawing series...")
+            .label("y = x^2")
+            .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &BLUEGREY));
+    
+        chart
+            .configure_series_labels()
+            .background_style(&background.mix(0.8))
+            .border_style(&WHITE)
+            .label_font(("sans-serif", 25).with_color(&foreground))
+            .draw()
+            .expect("error configuring...");
+    
+        root.present().expect("error presenting");
+        drop(chart);
+        drop(root);
+        
         Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,50 @@
-use Blinter::TcpReceiver;
 use slint::SharedPixelBuffer;
-use plotters::{prelude::*, style::full_palette::BLUEGREY};
+use std::net::TcpListener;
+use std::error::Error;
+use std::net::Ipv4Addr;
+use std::str::FromStr;
+use std::io::Write;
+use tcp_plotter::plot::call_plotter;
+// use tcp_plotter::tcp_receiver::{tcp_server, Cordinate};
 
 slint::slint! {
     export { MainWindow } from "ui/app-window.slint";
 }
 
 fn render_plot() -> slint::Image {
-    // TODO: add error handling rather than panic.
-    let foreground = RGBAColor(255,255,255,0.8);
-    let background = RGBAColor(40,40,40,1.0);
-
     let mut pixel_buffer = SharedPixelBuffer::new(1440, 960);
-    let size = (pixel_buffer.width(), pixel_buffer.height()); 
-    let backend = BitMapBackend::with_buffer(pixel_buffer.make_mut_bytes(), size);
-    
-    let root = backend.into_drawing_area();
-    root.fill(&background).expect("error filling drawing area");
-    let mut chart = ChartBuilder::on(&root)
-        .margin(20)
-        .x_label_area_size(80)
-        .y_label_area_size(80)
-        .build_cartesian_2d(-1f32..1f32, -0.1f32..1f32).expect("error building chart...");
-    
-    chart.configure_mesh().axis_style(&foreground)
-        .label_style(("sans-serif", 20).into_font().color(&foreground))
-        .bold_line_style(&foreground.mix(0.15))
-        .light_line_style(&foreground.mix(0.05))
-        .draw().expect("error drawing...");
-    
-    chart
-        .draw_series(LineSeries::new(
-            (-50..=50).map(|x| x as f32 / 50.0).map(|x| (x, x * x)),
-            ShapeStyle::from(&BLUEGREY).stroke_width(3),
-        )).expect("error drawing series...")
-        .label("y = x^2")
-        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &BLUEGREY));
-    
-    chart
-        .configure_series_labels()
-        .background_style(&background.mix(0.8))
-        .border_style(&WHITE)
-        .label_font(("sans-serif", 25).with_color(&foreground))
-        .draw().expect("error configuring...");
-    
-    root.present().expect("error presenting");
-    drop(chart);
-    drop(root); 
-
+    call_plotter(&mut pixel_buffer).expect("plot failed.");
     slint::Image::from_rgb8(pixel_buffer)
 }
 
-pub fn main() {
-    let main_window = MainWindow::new().unwrap();
+fn start_server(server_ip: slint::SharedString, listen_port: i32) {
+    println!("Acquiring...");
+    let server_ip: Ipv4Addr = Ipv4Addr::from_str(server_ip.as_str()).expect("Parse ipv4 addr failed!");
+    let listen_port: u16 = listen_port.try_into().expect("Port is not valid!");
+
+    let listener = TcpListener::bind((server_ip, listen_port)).unwrap();
+    let server = std::thread::spawn(move || {
+        let mut stream = listener.incoming().next().unwrap().unwrap();
+        stream.write("Hello World\n".as_bytes()).unwrap();
+    });
+
+    let slint_future = async move {
+    };
+
+    slint::spawn_local(async_compat::Compat::new(slint_future)).unwrap();
+    slint::run_event_loop_until_quit().unwrap();
+    server.join().unwrap();
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let main_window = MainWindow::new()?;
+    slint::set_xdg_app_id(main_window.get_appid())?;
+
+    // main_window.on_tcp_server(server);
+    main_window.on_tcp_server(start_server);
     main_window.on_render_plot(render_plot);
-    main_window.run().unwrap();
+
+    main_window.run()?;
+    
+    Ok(())
 }
